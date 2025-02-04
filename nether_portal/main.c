4ffe8eb8908bf3f8f448727d8d90b8fd3f79a666
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2025-02-04 16:55:49 +0800

    Update main.c

--- nether_portal/main.c
+++ nether_portal/main.c
@@ -1,26 +1,23 @@
-int main(){
-  // display 320*240
-while (1)
-	{
-		for (uint8_t u = 0; u < 29; u++)
-		{
-			for (uint8_t x = 0; x < 2; x++)
-			{
-				for (uint8_t y = 0; y < 3; y++)
-				{
-					for (uint8_t v = 0; v < 16; v++)
-					{
-						for (uint8_t w = 0; w < 16; w++)
-						{
-							//lcd_draw_point(v, w, frame_1[v][w]);
-							lcd_fill(v * 7.5 + (x * 120), w * 7.5 + (y * 120),
-									v * 7.5 + 7 + (x * 120),
-									w * 7.5 + 7 + (y * 120), frame[u][v][w]);
-						}
-					}
-				}
-			}
-		}
+#define uint8_t unsigned char
 
-	}
+int main(void)
+{
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    /*
+    320/16=20
+    240/16=15
+    16*5=80
+    320/80=4
+    240/80=3
+    */
+    while (1)
+        for (uint8_t u = 0; u < 29; u++)
+            for (uint8_t x = 0; x < 3; x++)
+                for (uint8_t y = 0; y < 4; y++)
+                    for (uint8_t v = 0; v < 16; v++)
+                        for (uint8_t w = 0; w < 16; w++)
+                            lcd_fill(v * 5 + (x * 80), w * 5 + (y * 80),
+                                     v * 5 + 7 + (x * 80),
+                                     w * 5 + 7 + (y * 80), frame[u][v][w]);
 }
