b39601cf981d61afd258fe6fd97f88f7100cd97a
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2025-02-07 02:13:34 +0800

    Update main.c

--- nether_portal/main.c
+++ nether_portal/main.c
@@ -1,9 +1,5 @@
-#define uint8_t unsigned char
-
 int main(void)
 {
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
     /*
     320/16=20
     240/16=15
@@ -13,11 +9,11 @@ int main(void)
     */
     while (1)
         for (uint8_t u = 0; u < 29; u++)
-            for (uint8_t x = 0; x < 3; x++)
-                for (uint8_t y = 0; y < 4; y++)
-                    for (uint8_t v = 0; v < 16; v++)
-                        for (uint8_t w = 0; w < 16; w++)
-                            lcd_fill(v * 5 + (x * 80), w * 5 + (y * 80),
-                                     v * 5 + 5 + (x * 80),
-                                     w * 5 + 5 + (y * 80), frame[u][v][w]);
+			for (uint8_t x = 0; x < 3; x++)
+				for (uint8_t y = 0; y < 4; y++)
+					for (uint8_t v = 0; v < 16; v++)
+						for (uint8_t w = 0; w < 16; w++)
+							lcd_fill(v * 5 + (x * 80), w * 5 + (y * 80),
+									v * 5 + 4 + (x * 80), w * 5 + 4 + (y * 80),
+									frame[u][v][w]);
 }
