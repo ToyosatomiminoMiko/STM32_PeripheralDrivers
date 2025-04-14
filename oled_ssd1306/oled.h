8196e92903298e1a7355b949fed483690e6044ae
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2025-04-14 19:10:50 +0800

    Update oled.h

--- oled_ssd1306/oled.h
+++ oled_ssd1306/oled.h
@@ -3,7 +3,7 @@
 
 #include "i2c.h"
 
-#define OLED_I2C hi2c2
+#define OLED_I2C hi2c1
 #define OLED_ADD 0x78 // 0b01111000
 
 extern uint8_t GRAM[8][128];
@@ -18,9 +18,8 @@ void oled_ShowFrame();
 void oled_SoftCirculation();
 void oled_SoftRolling();
 void oled_NewFrame();
-void oled_SetPixel(uint16_t x, uint16_t y);
 void oled_SetFrist();
-void oled_SetPoint(uint16_t x, uint16_t y);
+void oled_SetPoint(uint8_t x, uint8_t y);
 void oled_ProgressBar(uint8_t count);
 void oled_ShowNumber(uint8_t num, uint8_t position, uint8_t line);
 void oled_ShowLatin(uint8_t num, uint8_t position, uint8_t line);
@@ -35,7 +34,7 @@ void oled_ShowNumberTest();
 void oled_HW_rolling_stop();
 void oled_HW_rolling_start();
 void oled_HW_rolling();
-void oled_DrawCircle(uint16_t x0, uint16_t y0, uint8_t r);
+void oled_DrawCircle(uint8_t x0, uint8_t y0, uint8_t r);
 void oled_ShowImage(uint8_t *image);
 void oled_ShowChinese_16x16(uint8_t x0, uint8_t y0, uint8_t *ChineseChar);
 
